0942a60a5c42f1c47db93001eddbc4c471c83233
Tinapat Limsila <limsila.limsila@yahoo.com>
2022-10-25 15:50:49 +0700

    manual control

--- main.py
+++ main.py
@@ -16,40 +16,51 @@ loop checking the light gate
  """
 
 print('\033[0;33m' + "importing libraries" + '\033[0m')
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-import src.capture as capture
-import src.detect as detect
+#import src.capture as capture
+#import src.detect as detect
 
 
 doorpin = {
-    "plastic": 11,
-    "paper": 12,
-    "metal": 13,
-    "glass": 15,
+    "plastic": 19,
+    "paper": 13,
+    "metal": 6,
+    "glass": 5,
 }
-
-TEST = False
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setwarnings(False)
+TEST = True
+TEST_MECH = True
 # TEST = True
 
 
 print('\033[92m' + "Garbage detection is currently running......" + '\033[0m')
 while True:
     # if GPIO.input(16) == GPIO.HIGH:
-    if input("enter mf") == "":
+    #if input("enter mf") == "":
+    if True:
         time.sleep(2)
-        img_path = capture.Capture_Image()
-        print('\033[0;33m' + img_path + '\033[0m')
+        #img_path = capture.Capture_Image()
+        #print('\033[0;33m' + img_path + '\033[0m')
         if TEST == True:
             img_path = 'images/plastic_detected_works.png'
             # img_path = '/Users/tinapatlimsila/Desktop/Screenshots/Screen Shot 2565-10-23 at 21.11.55.png'
-        garbage_type = detect.run(source=img_path, weights="runs/train/results_3/weights/best.pt")
-        if garbage_type:
-            print('\033[0;33m' + garbage_type + '\033[0m')  
+            #garbage_type = detect.run(source=img_path, weights="runs/train/results_3/weights/best.pt")
         else:
-            print('\033[0;33m' + "no object detected" + '\033[0m')  
-
-        # GPIO.output(doorpin[garbage_type], GPIO.HIGH)
+            if garbage_type:
+                print('\033[0;33m' + garbage_type + '\033[0m')  
+            else:
+                print('\033[0;33m' + "no object detected" + '\033[0m')  
+        if TEST_MECH:
+            garbage_type = "plastic"
+            garbage_type = input("what type of garbage?: ")
+        GPIO.output(doorpin[garbage_type], GPIO.HIGH)
+        print("door opening")
         time.sleep(1)
-        # GPIO.output(doorpin[garbage_type], GPIO.LOW)
-    time.sleep(1)
+        GPIO.output(doorpin[garbage_type], GPIO.LOW)
+    time.sleep(2)
